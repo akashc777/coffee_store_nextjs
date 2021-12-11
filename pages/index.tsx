de75de57ff6540c650cdca5b9b16434b4069f3f1
Akash Hadagali <hey@akash.page>
2021-12-11 23:09:20 +0530

    added banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Banner from "../components/banner";
 
 const Home: NextPage = () => {
+    const handleOnBannerBtnClick = () => {
+        console.log("Hi banner button");
+    };
     return (
         <div className={styles.container}>
             <Head>
@@ -16,7 +20,10 @@ const Home: NextPage = () => {
             </Head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Coffee Connoisseur</h1>
+                <Banner
+                    buttonText="View store nearby"
+                    handleOnClick={handleOnBannerBtnClick}
+                />
             </main>
         </div>
     );
